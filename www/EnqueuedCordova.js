b867bcb37139ee482fd9f350b360a3d449f6ad47
Tom Hyde <thyde@neworbit.co.uk>
2017-03-13 17:24:51 +0000

    Fixed Queueing calls to exec

--- www/EnqueuedCordova.js
+++ www/EnqueuedCordova.js
@@ -6,9 +6,18 @@ var queue = q.resolve();
 var EnqueuedCordova = {};
 
 EnqueuedCordova.exec = function(success, error, service, action, args) {
-    queue = queue.then(function() { 
-        cordovaExec(success, error, service, action, args); 
-    });
+    var cordovaDeferred = q.defer();
+    cordovaExec(
+        function(result) { 
+            cordovaDeferred.resolve(result);
+            success(result);
+        }, 
+        function(result) { 
+            cordovaDeferred.resolve(result);
+            error(result);
+        }, 
+        service, action, args);
+    queue = queue.then(function() { return cordovaDeferred.promise; });
 }
 
 module.exports = EnqueuedCordova;
